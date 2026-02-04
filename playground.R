f0c264845e698d0c1b012a2e7adb5fcb3a2dfa64
Emil Ong <emilongeezy@gmail.com>
2026-02-04 21:24:39 +1100

    Completed entropy.html

--- playground.R
+++ playground.R
@@ -15,20 +15,55 @@ plot(X, - dnorm(vals, mean=0, sd=sd) * log(dnorm(vals, mean=0, sd=sd)),
      ylim = c(0,1))
 
 
+px <- seq(0,1,0.01)
+plot(px, -log(px),
+     type="l")
+
+
+
+
+## interactive
+
 
 
 library(plotly)
 
+X <- seq(-5, 5, 0.01)
+sds <- seq(0.5, 1, 0.05)
+
 fig <- plot_ly()
 
-for (s in sigmas) {
+for (s in sds) {
+  pdf <- dnorm(X, 0, s)
+  ent <- -pdf * log(pdf)
+  
+  # Add PDF line for this frame
   fig <- fig |>
-    add_lines(x = X, y = dnorm(X,0,s),
-              frame = round(s,2),
-              name = "PDF") |>
-    add_lines(x = X, y = -dnorm(X,0,s)*log(dnorm(X,0,s)),
-              frame = round(s,2),
-              name = "Entropy")
+    add_lines(
+      x = X, y = pdf,
+      type = "scatter", mode = "lines",
+      name = "PDF",
+      line = list(color = "blue"),
+      frame = paste0("s=", round(s,2))
+    )
+  
+  # Add Entropy line for the same frame
+  fig <- fig |>
+    add_lines(
+      x = X, y = ent,
+      type = "scatter", mode = "lines",
+      name = "Entropy",
+      line = list(color = "red"),
+      frame = paste0("s=", round(s,2))
+    )
 }
 
-fig |> layout(sliders = list(list(active = 0)))
+fig <- fig |> layout(
+  title = "Normal PDF and Entropy",
+  xaxis = list(title = "x"),
+  yaxis = list(title = "Value")
+) |> animation_opts(
+  frame = 100, transition = 0, redraw = FALSE
+)
+
+fig
